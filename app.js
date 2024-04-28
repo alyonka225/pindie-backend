59d81d28ab02070582088ec73325e510ad5c566b
alyonka225 <olyona06@mail.ru>
2024-04-29 00:38:48 +0700

    Added express

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const usersRouter = require("./routes/users");
+const gamesRouter = require("./routes/games");
+const categoriesRouter = require("./routes/categories");
+
+const PORT = 3000;
+const app = express();
+
+app.use(
+    bodyParser.json(),
+    express.static(path.join(__dirname, 'public')),
+    usersRouter,
+    gamesRouter,
+    categoriesRouter
+)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT https://localhost:${PORT}`);
+})
+
